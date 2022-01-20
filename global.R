01fcd60e66ae6186ac8241ccaa1f37bbc54a28b1
SumedhSankhe <sumedh.sankhe@gmail.com>
2022-01-19 18:32:41 -0800

    Added global variables

--- global.R
+++ global.R
@@ -11,3 +11,24 @@ library(ggplot2)
 source('home.R')
 source('loadData.R')
 source('analyse.R')
+
+
+PLANS <- c("Time of Use", "Tiered Rate Plan", "Solar & Renewable Energy Plan",
+           "Electric Vehicle Base Plan", "SmartRate Add-on")
+
+TIER <- list(
+  "Time of Use" = c("E-TOU-C", "E-TOU-D"),
+  "Tiered Rate Plan" = c("T1 (100% baseline)", "T2 (101%-400% baseline)",
+                         "T3 (> 400% baseline)"),
+  "Electric Vehicle Base Plan" = c('EV2-A', 'EV-B'),
+  "Solar & Renewable Energy Plan" = c("COMING-SOON"),
+  "SmartRate Add-on" = c("COMING-SOON")
+)
+
+HOUR <- list(
+  "E-TOU-C" = c(16,21),
+  "E-TOU-D" = c(17,20)
+)
+
+
+AGGCHOICE <- c('Day', 'Week','Month', 'Year')
